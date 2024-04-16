b5bd32dd0cbf67e8b6540ab2297b11a3c2951dd4
audry <audry.dupont@condorcet.be>
2024-04-16 23:57:49 +0200

    probleme caroussel

--- index_.php
+++ index_.php
@@ -7,10 +7,13 @@ require './admin/src/php/utils/liste_includes.php';
 <head>
     <title>ProjetTi</title>
     <meta charset="utf-8">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
             integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js"
+            integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous"
+            async></script>
     <script src="./admin/public/js/fonctions.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="./admin/public/css/style.css" type="text/css">
@@ -59,7 +62,7 @@ require './admin/src/php/utils/liste_includes.php';
 
 </div>
 
-
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
--- pages/accueil.php
+++ pages/accueil.php
@@ -1,8 +1,9 @@
 <main>
+
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body-tertiary">
         <div class="col-md-6 p-lg-5 mx-auto my-5">
-            <h1 class="display-3 fw-bold">Dupont Location</h1>
-            <h3 class="fw-normal text-muted mb-3">Location de matériels de bricolage et jardinage !</h3>
+            <h1 class="display-3 fw-bold">Location Dupont</h1>
+            <h3 class="fw-normal text-muted mb-3">Location de matériel de bricolage et de jardinage !</h3>
             <div class="d-flex gap-3 justify-content-center lead fw-normal">
                 <a class="icon-link" href="index_.php?page=a_propos.php">
                     À propos
@@ -13,34 +14,96 @@
         <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
     </div>
 
-
     <h1 class="text-center">Nos meilleures locations !</h1>
-    <br>
-    <br>
 
-        <div class="row d-flex justify-content-center ">
-            <div class="col-md-4 mx-2">
-                <div class="card me-3" style="width: 22rem;">
-                    <img src="admin/public/images/scieElec.jpg" class="card-img-top" alt="image1" style="max-width: 100%; height: auto">
-                    <div class="card-body">
-                        <h5 class="card-title">Notre scie électrique</h5>
-                        <p class="card-text">Elle vous sera utile dans bon nombre de vos tâches !</p>
-                        <a href="index_.php?page=connexion.php" class="btn btn-primary">Réserver</a>
-                    </div>
-                </div>
+    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="10000">
+                <img src="admin/public/images/tronco.jpg" class="d-block w-100" alt="dia1">
+
             </div>
-
-            <div class="col-md-4 mx-2">
-                <div class="card ms-3" style="width: 22rem;">
-                    <img src="admin/public/images/scieElec.jpg" class="card-img-top" alt="image1" style="max-width: 100%; height: auto">
-                    <div class="card-body">
-                        <h5 class="card-title">Notre scie électrique</h5>
-                        <p class="card-text">Elle vous sera utile dans bon nombre de vos tâches !</p>
-                        <a href="index_.php?page=connexion.php" class="btn btn-primary">Réserver</a>
-                    </div>
-                </div>
+            <div class="carousel-item" data-bs-interval="2000">
+                <img src="admin/public/images/stationPeinture.jpg" class="d-block w-100" alt="dia2">
             </div>
+            <div class="carousel-item">
+                <img src="admin/public/images/scieElec.jpg" class="d-block w-100" alt="dia3">
 
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+
+    <div class="container marketing">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
+                <h2 class="fw-normal">Titre</h2>
+                <p></p>
+            </div>
+            <div class="col-lg-4">
+                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
+                <h2 class="fw-normal">Autre Titre</h2>
+                <p></p>
+            </div>
+            <div class="col-lg-4">
+                <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"/></svg>
+                <h2 class="fw-normal">Autre titre</h2>
+                <p></p>
+            </div>
         </div>
 
+
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading fw-normal lh-1"> <span class="text-body-secondary"></span></h2>
+                <p class="lead"></p>
+            </div>
+            <div class="col-md-5">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7 order-md-2">
+                <h2 class="featurette-heading fw-normal lh-1"><span class="text-body-secondary">  </span></h2>
+                <p class="lead">
+            </div>
+            <div class="col-md-5 order-md-1">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading fw-normal lh-1"><span class="text-body-secondary">  </span></h2>
+                <p class="lead"></p>
+            </div>
+            <div class="col-md-5">
+                <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-bg)"/><text x="50%" y="50%" fill="var(--bs-secondary-color)" dy=".3em">500x500</text></svg>
+            </div>
+        </div>
+
+    </div>
 </main>
+
